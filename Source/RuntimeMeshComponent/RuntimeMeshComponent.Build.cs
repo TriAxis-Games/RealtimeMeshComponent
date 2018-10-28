b382a045343d86b6f3553222ce2f8b45702c744b
Matthew Laukala <mjlaukala@gmail.com>
2018-10-28 12:08:47 -0700

    Fix for missing references in 4.20

--- Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
+++ Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
@@ -1,5 +1,6 @@
 // Copyright 2016-2018 Chris Conway (Koderz). All Rights Reserved.
 
+using System.IO;
 using UnrealBuildTool;
 
 public class RuntimeMeshComponent : ModuleRules
@@ -8,20 +9,8 @@ public class RuntimeMeshComponent : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-        PublicIncludePaths.AddRange(
-            new string[] {
-                "RuntimeMeshComponent/Public"
-				// ... add public include paths required here ...
-			}
-            );
-
-
-        PrivateIncludePaths.AddRange(
-            new string[] {
-                "RuntimeMeshComponent/Private",
-				// ... add other private include paths required here ...
-			}
-            );
+        PublicIncludePaths.Add(Path.Combine(ModuleDirectory, "Public"));
+        PrivateIncludePaths.Add(Path.Combine(ModuleDirectory, "Private"));
 
 
         PublicDependencyModuleNames.AddRange(
