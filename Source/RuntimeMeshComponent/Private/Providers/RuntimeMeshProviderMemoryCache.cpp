e88cfd3bdd12a6db06b36da906c04ff7a01d839e
Chris Conway <me@koderz.me>
2019-12-01 15:04:23 -0500

    Added memory caching provider.

---
+++ Source/RuntimeMeshComponent/Private/Providers/RuntimeMeshProviderMemoryCache.cpp
@@ -0,0 +1,133 @@
+// Copyright 2016-2019 Chris Conway (Koderz). All Rights Reserved.
+
+
+#include "RuntimeMeshProviderMemoryCache.h"
+
+FRuntimeMeshProviderMemoryCacheProxy::FRuntimeMeshProviderMemoryCacheProxy(TWeakObjectPtr<URuntimeMeshProvider> InParent, const FRuntimeMeshProviderProxyPtr& InNextProvider)
+	: FRuntimeMeshProviderProxyPassThrough(InParent, InNextProvider)
+{
+}
+
+FRuntimeMeshProviderMemoryCacheProxy::~FRuntimeMeshProviderMemoryCacheProxy()
+{
+}
+
+void FRuntimeMeshProviderMemoryCacheProxy::ClearCache()
+{
+	CachedMeshData.Empty();
+	CachedCollisionSettings.Reset();
+	CachedHasCollisionMesh.Reset();
+	CachedCollisionData.Reset();
+}
+
+void FRuntimeMeshProviderMemoryCacheProxy::CreateSection(uint8 LODIndex, int32 SectionId, const FRuntimeMeshSectionProperties& SectionProperties)
+{
+	CachedMeshData.Remove(GenerateKeyFromLODAndSection(LODIndex, SectionId));
+	FRuntimeMeshProviderProxyPassThrough::CreateSection(LODIndex, SectionId, SectionProperties);
+}
+
+void FRuntimeMeshProviderMemoryCacheProxy::MarkSectionDirty(uint8 LODIndex, int32 SectionId)
+{
+	CachedMeshData.Remove(GenerateKeyFromLODAndSection(LODIndex, SectionId));
+	FRuntimeMeshProviderProxyPassThrough::MarkSectionDirty(LODIndex, SectionId);
+}
+
+void FRuntimeMeshProviderMemoryCacheProxy::RemoveSection(uint8 LODIndex, int32 SectionId)
+{
+	CachedMeshData.Remove(GenerateKeyFromLODAndSection(LODIndex, SectionId));
+	FRuntimeMeshProviderProxyPassThrough::RemoveSection(LODIndex, SectionId);
+}
+
+void FRuntimeMeshProviderMemoryCacheProxy::MarkCollisionDirty()
+{
+	CachedCollisionSettings.Reset();
+	CachedHasCollisionMesh.Reset();
+	CachedCollisionData.Reset();
+	FRuntimeMeshProviderProxyPassThrough::MarkCollisionDirty();
+}
+
+bool FRuntimeMeshProviderMemoryCacheProxy::GetSectionMeshForLOD(uint8 LODIndex, int32 SectionId, FRuntimeMeshRenderableMeshData& MeshData)
+{
+	TOptional<FRuntimeMeshRenderableMeshData>* CachedData = CachedMeshData.Find(GenerateKeyFromLODAndSection(LODIndex, SectionId));
+	if (CachedData)
+	{
+		if (CachedData->IsSet())
+		{
+			MeshData = CachedData->GetValue();
+		}
+		return CachedData->IsSet();
+	}
+
+	auto Temp = NextProvider.Pin();
+	if (Temp.IsValid())
+	{
+		bool bHasData = Temp->GetSectionMeshForLOD(LODIndex, SectionId, MeshData);
+		TOptional<FRuntimeMeshRenderableMeshData>& NewCache = CachedMeshData.FindOrAdd(GenerateKeyFromLODAndSection(LODIndex, SectionId));
+		if (bHasData)
+		{
+			NewCache = MeshData;
+		}
+		return bHasData;
+	}
+	return false;
+}
+
+FRuntimeMeshCollisionSettings FRuntimeMeshProviderMemoryCacheProxy::GetCollisionSettings()
+{
+	if (CachedCollisionSettings.IsSet())
+	{
+		return CachedCollisionSettings.Get(FRuntimeMeshCollisionSettings());
+	}
+
+	auto Temp = NextProvider.Pin();
+	if (Temp.IsValid())
+	{
+		FRuntimeMeshCollisionSettings NewSettings = Temp->GetCollisionSettings();
+		CachedCollisionSettings = NewSettings;
+		return NewSettings;
+	}
+	return FRuntimeMeshCollisionSettings();
+}
+
+bool FRuntimeMeshProviderMemoryCacheProxy::HasCollisionMesh()
+{
+	if (CachedHasCollisionMesh.IsSet())
+	{
+		return CachedHasCollisionMesh.Get(false);
+	}
+
+	auto Temp = NextProvider.Pin();
+	if (Temp.IsValid())
+	{
+		bool bHasData = Temp->HasCollisionMesh();
+		CachedHasCollisionMesh = bHasData;
+		return bHasData;
+	}
+	return false;
+}
+
+bool FRuntimeMeshProviderMemoryCacheProxy::GetCollisionMesh(FRuntimeMeshCollisionData& CollisionData)
+{
+	if (CachedCollisionData.IsSet())
+	{		
+		CollisionData = CachedCollisionData->Value;
+		return CachedCollisionData->Key;
+	}
+
+	auto Temp = NextProvider.Pin();
+	if (Temp.IsValid())
+	{
+		bool bHasData = Temp->GetCollisionMesh(CollisionData);
+		CachedCollisionData = TPair<bool, FRuntimeMeshCollisionData>(bHasData, CollisionData);
+		return bHasData;
+	}
+	return false;
+}
+
+bool FRuntimeMeshProviderMemoryCacheProxy::IsThreadSafe() const
+{
+	// Since all we're doing is acting as an intermediary cache with no ties to anything
+	// it's perfectly safe for this to be multithreaded. The RMC guarantees to never 
+	// call the same section on multiple threads at the same time
+	return true;
+}
---
+++ Source/RuntimeMeshComponent/Public/Providers/RuntimeMeshProviderMemoryCache.h
@@ -0,0 +1,64 @@
+// Copyright 2016-2019 Chris Conway (Koderz). All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RuntimeMeshProvider.h"
+#include "RuntimeMeshProviderMemoryCache.generated.h"
+
+
+class RUNTIMEMESHCOMPONENT_API FRuntimeMeshProviderMemoryCacheProxy : public FRuntimeMeshProviderProxyPassThrough
+{
+	FORCEINLINE int64 GenerateKeyFromLODAndSection(int32 LODIndex, int32 SectionId)
+	{
+		return (((int64)LODIndex) << 32) | ((int64)SectionId);
+	}
+
+
+	TMap<int64, TOptional<FRuntimeMeshRenderableMeshData>> CachedMeshData;
+
+	TOptional<FRuntimeMeshCollisionSettings> CachedCollisionSettings;
+	TOptional<bool> CachedHasCollisionMesh;
+	TOptional<TPair<bool, FRuntimeMeshCollisionData>> CachedCollisionData;
+public:
+	FRuntimeMeshProviderMemoryCacheProxy(TWeakObjectPtr<URuntimeMeshProvider> InParent, const FRuntimeMeshProviderProxyPtr& InNextProvider);
+	~FRuntimeMeshProviderMemoryCacheProxy();
+
+	void ClearCache();
+
+protected:
+	virtual void CreateSection(uint8 LODIndex, int32 SectionId, const FRuntimeMeshSectionProperties& SectionProperties);
+	virtual void MarkSectionDirty(uint8 LODIndex, int32 SectionId);
+	virtual void RemoveSection(uint8 LODIndex, int32 SectionId);
+	virtual void MarkCollisionDirty();
+
+
+
+	virtual bool GetSectionMeshForLOD(uint8 LODIndex, int32 SectionId, FRuntimeMeshRenderableMeshData& MeshData) override;
+
+	FRuntimeMeshCollisionSettings GetCollisionSettings() override;
+	bool HasCollisionMesh() override;
+	virtual bool GetCollisionMesh(FRuntimeMeshCollisionData& CollisionData) override;
+	
+	bool IsThreadSafe() const override;
+
+};
+
+UCLASS(HideCategories = Object, BlueprintType)
+class RUNTIMEMESHCOMPONENT_API URuntimeMeshProviderMemoryCache : public URuntimeMeshProvider
+{
+	GENERATED_BODY()
+
+public:
+
+	UPROPERTY(EditAnywhere)
+	URuntimeMeshProvider* SourceProvider;
+
+protected:
+	virtual FRuntimeMeshProviderProxyRef GetProxy() override 
+	{ 
+		FRuntimeMeshProviderProxyPtr SourceProviderProxy = SourceProvider ? SourceProvider->SetupProxy() : FRuntimeMeshProviderProxyPtr();
+
+		return MakeShared<FRuntimeMeshProviderMemoryCacheProxy, ESPMode::ThreadSafe>(TWeakObjectPtr<URuntimeMeshProvider>(this), SourceProviderProxy);
+	}
+};
