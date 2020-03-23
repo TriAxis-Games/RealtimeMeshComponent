94459fae5bfdcaa24b7db21aacc45926fe5adc01
Moddingear <moddingear@gmail.com>
2020-03-23 12:33:50 +0100

    Edge case correction to allow for 256*256 planes to still use 16 bit indices

--- Source/RuntimeMeshComponent/Private/Providers/RuntimeMeshProviderPlane.cpp
+++ Source/RuntimeMeshComponent/Private/Providers/RuntimeMeshProviderPlane.cpp
@@ -61,7 +61,7 @@ void FRuntimeMeshProviderPlaneProxy::UpdateProxyParameters(URuntimeMeshProvider*
 				Properties.bIsVisible = true;
 				Properties.MaterialSlot = 0;
 				Properties.UpdateFrequency = ERuntimeMeshUpdateFrequency::Infrequent;
-				Properties.bWants32BitIndices = VertsAB[LODIndex] * VertsAC[LODIndex] >= 1 << 16; //Use 32 bit indices if more than 2^16 verts are needed
+				Properties.bWants32BitIndices = VertsAB[LODIndex] * VertsAC[LODIndex] > 1 << 16; //Use 32 bit indices if more than 2^16 verts are needed
 
 				CreateSection(LODIndex, 0, Properties);
 			}
