d53d0744ed441a177c35d37d373747f1998b3254
Chris Conway <chris@koderz.me>
2020-04-25 13:38:53 -0400

    Fixed include in RuntimeMeshModifierNormals.cpp

--- Source/RuntimeMeshComponent/Private/Modifiers/RuntimeMeshModifierNormals.cpp
+++ Source/RuntimeMeshComponent/Private/Modifiers/RuntimeMeshModifierNormals.cpp
@@ -1,7 +1,7 @@
 // Copyright 2016-2020 Chris Conway (Koderz). All Rights Reserved.
 
 
-#include "RuntimeMeshModifierNormals.h"
+#include "Modifiers/RuntimeMeshModifierNormals.h"
 
 
 URuntimeMeshModifierNormals::URuntimeMeshModifierNormals()
