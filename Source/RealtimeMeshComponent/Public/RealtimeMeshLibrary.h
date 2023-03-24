594ea659cbd8183f544be625883dd4ad5d0f2a7c
Ruben Moor <ruben.moor@gmail.com>
2023-03-23 19:37:45 -0500

    AppendCapsuleMesh header file

--- Source/RealtimeMeshComponent/Public/RealtimeMeshLibrary.h
+++ Source/RealtimeMeshComponent/Public/RealtimeMeshLibrary.h
@@ -3,6 +3,8 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "IndexTypes.h"
+#include "IntVectorTypes.h"
 #include "RealtimeMeshSimple.h"
 #include "Data/RealtimeMeshConfig.h"
 #include "Kismet/BlueprintFunctionLibrary.h"
@@ -37,8 +39,31 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "RealtimeMesh|MeshGeneration")
 	static FRealtimeMeshSimpleMeshData& AppendBoxMesh(FVector BoxRadius, FTransform BoxTransform, UPARAM(Ref) FRealtimeMeshSimpleMeshData& MeshData);
 
-	/** Generate vertex and index buffer for a simple box, given the supplied dimensions. Normals, UVs and tangents are also generated for each vertex. */
+	/** Generate vertex and index buffer for a capsule. */
+	UFUNCTION(BlueprintCallable, Category = "RealtimeMesh|MeshGeneration")
+	static FRealtimeMeshSimpleMeshData& AppendCapsuleMesh
+		( UPARAM(Ref) FRealtimeMeshSimpleMeshData& MeshData
+		, const FTransform& Transform
+		, float Radius
+		, float CylinderLength
+		, int32 HemisphereSteps
+		, int32 CircleSteps
+		, int32 CylinderSteps
+		);
+
 	UFUNCTION(BlueprintCallable, Category = "RealtimeMesh|MeshGeneration", meta=(AutoCreateRefTerm="Transform"))
 	static FRealtimeMeshSimpleMeshData& AppendMesh(UPARAM(Ref) FRealtimeMeshSimpleMeshData& TargetMeshData, const FRealtimeMeshSimpleMeshData& MeshDataToAdd, const FTransform& Transform);
+
+private:
+	static FVector3d SphericalToCartesian(double r, double theta, double phi)
+	{
+		const double Sphi = sin(phi);
+		const double Cphi = cos(phi);
+		const double Ctheta = cos(theta);
+		const double Stheta = sin(theta);
+
+		return FVector3d(r * Ctheta * Sphi, r * Stheta * Sphi, r * Cphi);
+	}
 	
+
 };
