5171ab15d769455491c6f600230639dea106f89f
Chris Conway <chris@koderz.io>
2024-06-23 17:42:52 -0400

    Updating stress test actor to show how to use multiple sections without using polygroups.

--- Source/RealtimeMeshTests/Private/FunctionalTests/RealtimeMeshStressTestActor.cpp
+++ Source/RealtimeMeshTests/Private/FunctionalTests/RealtimeMeshStressTestActor.cpp
@@ -18,35 +18,27 @@ ARealtimeMeshStressTestActor::ARealtimeMeshStressTestActor()
 
 static const auto SectionGroupKey = FRealtimeMeshSectionGroupKey::Create(0, FName("TestTripleBox"));
 
+static const auto Section0Key = FRealtimeMeshSectionKey::Create(SectionGroupKey, FName("Section0"));
+static const auto Section1Key = FRealtimeMeshSectionKey::Create(SectionGroupKey, FName("Section1"));
+static const auto Section2Key = FRealtimeMeshSectionKey::Create(SectionGroupKey, FName("Section2"));
+
 void ARealtimeMeshStressTestActor::OnGenerateMesh_Implementation()
 {
 	Super::OnGenerateMesh_Implementation();
 
 	// Initialize the simple mesh
 	URealtimeMeshSimple* RealtimeMesh = GetRealtimeMeshComponent()->InitializeRealtimeMesh<URealtimeMeshSimple>();
-	
+
 	// Setup the two material slots
 	RealtimeMesh->SetupMaterialSlot(0, "PrimaryMaterial", UMaterial::GetDefaultMaterial(EMaterialDomain::MD_Surface));
 	RealtimeMesh->SetupMaterialSlot(1, "SecondaryMaterial", UMaterial::GetDefaultMaterial(EMaterialDomain::MD_Surface));
 
-	FRealtimeMeshStreamSet StreamSet;	
-	// Here we're using the builder just to initialize the stream set
-	TRealtimeMeshBuilderLocal<uint16, FPackedNormal, FVector2DHalf, 2> Builder(StreamSet);
-	Builder.EnableTangents();
-	Builder.EnableTexCoords();
-	Builder.EnableColors();
-	Builder.EnablePolyGroups();
+	// Setup empty structure
+	RealtimeMesh->CreateSectionGroup(SectionGroupKey, false);
+	RealtimeMesh->CreateSection(Section0Key, FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Dynamic, 0), FRealtimeMeshStreamRange(), false);
+	RealtimeMesh->CreateSection(Section1Key, FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Dynamic, 1), FRealtimeMeshStreamRange(), false);
+	RealtimeMesh->CreateSection(Section2Key, FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Dynamic, 2), FRealtimeMeshStreamRange(), false);
 
-	URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 100.0f, 200.0f), FTransform3f::Identity, 2, FColor::White);
-	URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(200.0f, 100.0f, 100.0f), FTransform3f::Identity, 1, FColor::White);
-	URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 200.0f, 100.0f), FTransform3f::Identity, 3, FColor::White);
-
-	RealtimeMesh->CreateSectionGroup(SectionGroupKey, StreamSet);
-
-	RealtimeMesh->UpdateSectionConfig(FRealtimeMeshSectionKey::CreateForPolyGroup(SectionGroupKey, 1), FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Static, 0));
-	RealtimeMesh->UpdateSectionConfig(FRealtimeMeshSectionKey::CreateForPolyGroup(SectionGroupKey, 2), FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Dynamic, 0));
-	RealtimeMesh->UpdateSectionConfig(FRealtimeMeshSectionKey::CreateForPolyGroup(SectionGroupKey, 3), FRealtimeMeshSectionConfig(ERealtimeMeshSectionDrawType::Static, 1));
-		
 }
 
 void ARealtimeMeshStressTestActor::TickActor(float DeltaTime, ELevelTick TickType, FActorTickFunction& ThisTickFunction)
@@ -63,6 +55,7 @@ void ARealtimeMeshStressTestActor::TickActor(float DeltaTime, ELevelTick TickTyp
 
 		TPromise<TSharedPtr<TStrongObjectPtr<URealtimeMeshSimple>>> Promise;
 		PendingGeneration = Promise.GetFuture();
+
 		AsyncTask(ENamedThreads::AnyThread, [RealtimeMeshRef, Promise = MoveTemp(Promise)]() mutable
 		{
 			FRealtimeMeshStreamSet StreamSet;
@@ -71,18 +64,38 @@ void ARealtimeMeshStressTestActor::TickActor(float DeltaTime, ELevelTick TickTyp
 			Builder.EnableTangents();
 			Builder.EnableTexCoords();
 			Builder.EnableColors();
-			Builder.EnablePolyGroups();
 
 			const float Scale = (FMath::Cos(FPlatformTime::Seconds() * PI) * 0.25f) + 0.5f;
 			const FTransform3f Transform = FTransform3f(FQuat4f::Identity, FVector3f(0, 0, 0), FVector3f(Scale, Scale, Scale));
 
-			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 100.0f, 200.0f), Transform, 2, FColor::White);
-			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(200.0f, 100.0f, 100.0f), Transform, 1, FColor::White);
-			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 200.0f, 100.0f), Transform, 3, FColor::White);
 
-			RealtimeMeshRef.Get()->UpdateSectionGroup(SectionGroupKey, StreamSet);
-	RealtimeMeshRef.Get()->SetupMaterialSlot(0, "PrimaryMaterial", UMaterial::GetDefaultMaterial(EMaterialDomain::MD_Surface));
-	RealtimeMeshRef.Get()->SetupMaterialSlot(1, "SecondaryMaterial", UMaterial::GetDefaultMaterial(EMaterialDomain::MD_Surface));
+			int32 Section0StartVertex = Builder.NumVertices();
+			int32 Section0StartTriangle = Builder.NumTriangles();			
+			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 100.0f, 200.0f), Transform, 2, FColor::White);
+			
+			int32 Section1StartVertex = Builder.NumVertices();
+			int32 Section1StartTriangle = Builder.NumTriangles();
+			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(200.0f, 100.0f, 100.0f), Transform, 1, FColor::White);
+			
+			int32 Section2StartVertex = Builder.NumVertices();
+			int32 Section2StartTriangle = Builder.NumTriangles();
+			URealtimeMeshBasicShapeTools::AppendBoxMesh(StreamSet, FVector3f(100.0f, 200.0f, 100.0f), Transform, 3, FColor::White);
+			
+			int32 FinalNumVertices = Builder.NumVertices();
+			int32 FinalNumTriangles = Builder.NumTriangles();
+
+			FRealtimeMeshStreamRange Section0Range(Section0StartVertex, Section1StartVertex, Section0StartTriangle * 3, Section1StartTriangle * 3);
+			FRealtimeMeshStreamRange Section1Range(Section1StartVertex, Section2StartVertex, Section1StartTriangle * 3, Section2StartTriangle * 3);
+			FRealtimeMeshStreamRange Section2Range(Section2StartVertex, FinalNumVertices, Section2StartTriangle * 3, FinalNumTriangles * 3);
+			
+
+			RealtimeMeshRef.Get()->UpdateSectionGroup(SectionGroupKey, MoveTemp(StreamSet));
+
+			// Update the ranges for all the sections.
+			RealtimeMeshRef.Get()->UpdateSectionRange(Section0Key, Section0Range);
+			RealtimeMeshRef.Get()->UpdateSectionRange(Section1Key, Section1Range);
+			RealtimeMeshRef.Get()->UpdateSectionRange(Section2Key, Section2Range);
+			
 			Promise.EmplaceValue(RealtimeMeshRef);
 		});
 		
